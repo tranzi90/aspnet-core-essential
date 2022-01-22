7a3381a229ac8f7f1fe07007bd015e4ec2a1a076
denis_levchenko <tranzi90@gmail.com>
2022-01-22 20:24:59 +0300

    mvc example

---
+++ 001.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _001.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
